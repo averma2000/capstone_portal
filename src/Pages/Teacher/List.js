d8ad9eaf8937c723cf9f10624f8974b647e1313c
anuj <anuj.verma255@gmail.com>
2023-05-01 14:09:43 +0530

    teacher dashboard complete

--- src/Pages/Teacher/List.js
+++ src/Pages/Teacher/List.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 const List = () => {
-    return (<div className="page">
-        <h1>Welcome to List page</h1>
+	return (
+		<div className="page">
+			<h1>Welcome to List page</h1>
 
-        <h3>this page will display list of all students who a under the mentorship of the specific teacher</h3>
-    </div>);
-    
+			<h3>
+				this page will display list of all students who a under the mentorship
+				of the specific teacher
+			</h3>
+		</div>
+	);
 };
 
 export default List;
